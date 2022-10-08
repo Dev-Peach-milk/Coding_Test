7c3e4ed1d092e053ab00232f1c1ace42325884f0
peachmilk24 <112180368+peachmilk24@users.noreply.github.com>
2022-10-09 00:08:52 +0900

    updated at 2022. 10. 9. 오전 12:08:52

--- programmers/하샤드 수/solution.js
+++ programmers/하샤드 수/solution.js
@@ -3,7 +3,14 @@
 function solution(x) {
     var answer = true;
     let sum = 0;
+    /*1차 나의 풀이
+    let num = x.toString().split('');
+    for(let i = 0; i < num.length; i++) {
+        sum += parseInt(num[i]);
+    }
+    */
+    //다른 사람 풀이 참고 -> reduce 사용함, 더할때 +를 앞에 붙여서 문자열을 숫자
     let num = x.toString().split('').reduce((acc, cur) => +acc + +cur);
     if(x % num !== 0) answer = false;
     return answer;
-}
+};
