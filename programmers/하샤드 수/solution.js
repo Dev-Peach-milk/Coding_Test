0090d35790d6e28182a4e9763700e7aea76f9abd
peachmilk24 <112180368+peachmilk24@users.noreply.github.com>
2022-10-09 00:06:01 +0900

    updated at 2022. 10. 9. 오전 12:06:00

--- programmers/하샤드 수/solution.js
+++ programmers/하샤드 수/solution.js
@@ -1,13 +1,9 @@
-// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12947
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12947?language=javascript
 
 function solution(x) {
     var answer = true;
     let sum = 0;
-    let num = x.toString().split('');
-    for(let i = 0; i < num.length; i++) {
-        sum += parseInt(num[i]);
-    }
-    console.log(x, num, sum)
-    if(x % sum !== 0) answer = false;
+    let num = x.toString().split('').reduce((acc, cur) => +acc + +cur);
+    if(x % num !== 0) answer = false;
     return answer;
 }
