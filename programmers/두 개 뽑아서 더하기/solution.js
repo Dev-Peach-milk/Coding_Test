fab5fbd59ce9215eff4f8db12092b5f198c36870
peachmilk24 <112180368+peachmilk24@users.noreply.github.com>
2022-10-09 00:33:02 +0900

    updated at 2022. 10. 9. 오전 12:33:02

--- programmers/두 개 뽑아서 더하기/solution.js
+++ programmers/두 개 뽑아서 더하기/solution.js
@@ -1,6 +1,7 @@
-// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/68644
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=javascript
 
 function solution(numbers) {
+    //1차 나의 풀이
     var answer = [];
     for(let i = 0; i < numbers.length - 1; i++) {
         let sum = 0;
@@ -13,3 +14,18 @@ function solution(numbers) {
     return answer;
 }
 
+/*인상적인 다른 풀이 -> spread연산자와 set을 사용함
+function solution(numbers) {
+    const temp = []
+
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            temp.push(numbers[i] + numbers[j])
+        }
+    }
+
+    const answer = [...new Set(temp)]
+
+    return answer.sort((a, b) => a - b)
+}
+*/
