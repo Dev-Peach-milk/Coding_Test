d67c7a908f1353852be36b8d6c26c8f19e5fa7a0
peachmilk24 <112180368+peachmilk24@users.noreply.github.com>
2022-10-27 11:19:49 +0900

    updated at 2022. 10. 27. 오전 11:19:49

--- baekjoon/1158/solution.js
+++ baekjoon/1158/solution.js
@@ -8,42 +8,14 @@ let wordInput = fs.readFileSync(filePath).toString().trim().split(/\s/g);
 let N = +wordInput[0];
 let K = +wordInput[1];
 ​
-class Circle {
-  constructor(n) {
-    this.array = new Array(n);
-    this.size = n;
-    this.length = 0;
-    this.head = 0;
-    this.tail = 0;
+let arr = new Array(N).fill(1).map((e, i) => i + 1);
+let answer = [];
+while (arr.length) {
+  for (let i = 0; i < K; i++) {
+    arr.push(arr.shift());
   }
-​
-  enqueue(num) {
-    this.length++;
-    this.array[this.tail++ % this.size] = num;
-  }
-​
-  dequeue() {
-    this.length--;
-    return this.array[this.head++ % this.size];
-  }
+  answer.push(arr.pop());
 }
-​
-let circle = new Circle(N);
-for (let i = 1; i <= N; i++) {
-  circle.enqueue(i);
-}
-​
-let remove = [];
-circle.tail = circle.head = K - 1;
-remove.push(circle.dequeue());
-while (circle.length != 0) {
-  let count = K - 1;
-  while (count--) {
-    circle.enqueue(circle.dequeue());
-  }
-  remove.push(circle.dequeue());
-}
-​
-remove = remove.join(", ");
-remove = "<" + remove + ">";
-console.log(remove);
+answer = answer.join(", ");
+answer = "<" + answer + ">";
+console.log(answer);
