1c8ad2fd5c359ab5649d2194af9bd635e814415b
peachmilk24 <112180368+peachmilk24@users.noreply.github.com>
2022-11-01 10:12:05 +0900

    updated at 2022. 11. 1. 오전 10:12:05

--- baekjoon/10799/solution.js
+++ baekjoon/10799/solution.js
@@ -7,7 +7,7 @@ let lineInput = fs.readFileSync(filePath).toString().trim().split("\n");
 let input = lineInput[0];
 let stick = [];
 let count = 0;
-for (let i in input) {
+for (let i = 0; i < input.length; i++) {
   if (input[i] === "(") {
     stick.push(input[i]);
   } else {
