c7bb25bde77db8b2aa14bcdc0406053cc8120951
peachmilk24 <112180368+peachmilk24@users.noreply.github.com>
2022-11-01 10:04:14 +0900

    uploaded at 2022. 11. 1. 오전 10:04:14

---
+++ baekjoon/10799/solution.js
@@ -0,0 +1,23 @@
+// [문제 링크]: https://www.acmicpc.net/problem/10799
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+​
+let lineInput = fs.readFileSync(filePath).toString().trim().split("\n");
+let input = lineInput[0];
+let stick = [];
+let count = 0;
+for (let i in input) {
+  if (input[i] === "(") {
+    stick.push(input[i]);
+  } else {
+    if (input[i - 1] === "(") {
+      stick.pop();
+      count += stick.length;
+    } else {
+      stick.pop();
+      count++;
+    }
+  }
+}
+console.log(count);
