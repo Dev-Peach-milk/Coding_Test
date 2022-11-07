e1aed175517b9f8e53affd1337b035ed2a842bac
peachmilk24 <112180368+peachmilk24@users.noreply.github.com>
2022-11-07 10:41:10 +0900

    updated at 2022. 11. 7. 오전 10:41:09

--- baekjoon/1918/solution.js
+++ baekjoon/1918/solution.js
@@ -1,33 +1,38 @@
 // [문제 링크]: https://www.acmicpc.net/problem/1918
 
 const fs = require("fs");
-const stdin = (process.platform === "linux" ? fs.readFileSync("/dev/stdin") : `A+B*C*((D-E)*G)`).toString().trim().split("\n");
-const input = (() => {
-    let line = 0;
-    return () => stdin[line++];
-})();
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
 ​
-const infix = input().split("");
-let postfix = "";
+let str = input[0];
 let stack = [];
-​
-for (let i = 0; i < infix.length; i++) {
-    let ch = infix[i];
-​
-    if (ch >= "A" && ch <= "Z") postfix += ch;
-    else if (ch === "(") stack.push(ch);
-    else if (ch === ")") {
-        while (stack.length && stack[stack.length - 1] !== "(") postfix += stack.pop();
-        stack.pop();
-    } else if (ch === "+" || ch === "-") {
-        while (stack.length && stack[stack.length - 1] !== "(") postfix += stack.pop();
-        stack.push(ch);
-    } else if (ch === "*" || ch === "/") {
-        while (stack.length && (stack[stack.length - 1] === "*" || stack[stack.length - 1] === "/")) postfix += stack.pop();
-        stack.push(ch);
+let ans = "";
+for (let i = 0; i < str.length; i++) {
+  if (str[i] >= "A" && str[i] <= "Z") {
+    ans += str[i];
+  } else if (str[i] === "(") {
+    stack.push(str[i]);
+  } else if (str[i] === ")") {
+    while (stack.length && stack[stack.length - 1] !== "(") {
+      ans += stack.pop();
     }
+    stack.pop(); // ( 지워주기
+  } else if (str[i] === "+" || str[i] === "-") {
+    while (stack.length && stack[stack.length - 1] !== "(") {
+      ans += stack.pop();
+    }
+    stack.push(str[i]);
+  } else if (str[i] === "*" || str[i] === "/") {
+    while (
+      (stack.length && stack[stack.length - 1] === "*") ||
+      stack[stack.length - 1] === "/"
+    ) {
+      ans += stack.pop();
+    }
+    stack.push(str[i]);
+  }
 }
-​
-while (stack.length) postfix += stack.pop();
-​
-console.log(postfix);
+while (stack.length) {
+  ans += stack.pop();
+}
+console.log(ans);
