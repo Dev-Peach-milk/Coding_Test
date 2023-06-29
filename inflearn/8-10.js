e85273a657d51eb2218cef7c516724b3591db461
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-06-29 20:42:10 +0900

    답안으로 수정

--- inflearn/8-10.js
+++ inflearn/8-10.js
@@ -1,5 +1,33 @@
+// 내 답 첫번째
+// function solution(m, arr) {
+//   let answer = [];
+//   let tmp = Array.from({ length: m }, () => 0);
+
+//   function DFS(L) {
+//     if (L === m) {
+//       answer.push([...tmp].join(" "));
+//       return;
+//     }
+
+//     for (let i = 0; i < arr.length; i++) {
+//       if (L === 0) tmp = Array.from({ length: m }, () => 0);
+//       if (tmp.includes(arr[i])) continue;
+
+//       tmp[L] = arr[i];
+//       DFS(L + 1);
+//     }
+//   }
+
+//   DFS(0);
+//   answer.push(answer.length);
+//   answer = answer.join("\n");
+//   return answer;
+// }
+
 function solution(m, arr) {
   let answer = [];
+  const n = arr.length;
+  let ch = Array.from({ length: n }, () => 0);
   let tmp = Array.from({ length: m }, () => 0);
 
   function DFS(L) {
@@ -7,13 +35,13 @@ function solution(m, arr) {
       answer.push([...tmp].join(" "));
       return;
     }
-
-    for (let i = 0; i < arr.length; i++) {
-      if (L === 0) tmp = Array.from({ length: m }, () => 0);
-      if (tmp.includes(arr[i])) continue;
-
-      tmp[L] = arr[i];
-      DFS(L + 1);
+    for (let i = 0; i < n; i++) {
+      if (ch[i] === 0) {
+        ch[i] = 1;
+        tmp[L] = arr[i];
+        DFS(L + 1);
+        ch[i] = 0;
+      }
     }
   }
 
@@ -27,27 +55,27 @@ let arr = [3, 6, 9];
 console.log(solution(2, arr));
 
 /**
- * function solution(m, arr){         
-                let answer=[];
-                n=arr.length;
-                let ch=Array.from({length:n}, ()=>0);
-                let tmp=Array.from({length:m}, ()=>0);;
-                function DFS(L){
-                    if(L===m){
-                        answer.push(tmp.slice()); 
-                    }
-                    else{
-                        for(let i=0; i<n; i++){
-                            if(ch[i]===0){
-                                ch[i]=1;
-                                tmp[L]=arr[i];
-                                DFS(L+1);
-                                ch[i]=0;
-                            }
-                        }
-                    }
-                }
-                DFS(0);
-                return answer;
-            }
+ * function solution(m, arr){
+ *  let answer=[];
+ *  n=arr.length;
+ *  let ch=Array.from({length:n}, ()=>0);
+ *  let tmp=Array.from({length:m}, ()=>0);
+ *  function DFS(L){
+ *    if(L===m){
+ *      answer.push(tmp.slice());
+ *    }
+ *    else{
+ *      for(let i=0; i<n; i++){
+ *        if(ch[i]===0){
+ *          ch[i]=1;
+ *          tmp[L]=arr[i];
+ *          DFS(L+1);
+ *          ch[i]=0;
+ *        }
+ *      }
+ *    }
+ *  }
+ *  DFS(0);
+ *  return answer;
+ * }
  */
