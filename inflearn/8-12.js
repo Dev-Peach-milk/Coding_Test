4e7ebe0b118e869a447b81685e90283ff656ba9c
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-06-30 10:56:11 +0900

    조합 메모이제이션 사용

--- inflearn/8-12.js
+++ inflearn/8-12.js
@@ -1,7 +1,7 @@
 // 조합
 
 // 처음 나의 풀이
-function solution(n, r) {
+function solution1(n, r) {
   let answer;
 
   function DFS(n, r) {
@@ -9,6 +9,22 @@ function solution(n, r) {
     if (r === n) return 1; // 이 두개를 합쳐서 if(n === r || r === 0) return 1; 로 할 수 있음
 
     return DFS(n - 1, r - 1) + DFS(n - 1, r);
+  }
+
+  answer = DFS(n, r);
+  return answer;
+}
+
+// 메모이제이션 사용
+function solution(n, r) {
+  let answer;
+  let dy = Array.from(Array(35), () => Array(35).fill(0)); // 문제 조건의 n 크기 따라 결정
+
+  function DFS(n, r) {
+    if (dy[n][r] > 0) return dy[n][r]; // 메모이제이션 된 값이 있으면 사용하기
+    if (n === r || r === 0) return 1;
+
+    return (dy[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r)); // 메모이제이션
   }
 
   answer = DFS(n, r);
