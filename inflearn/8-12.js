505fa6b58cee40528c7b71b9585fecbdd3ec6df8
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-06-30 10:43:21 +0900

    return 조건 수정

--- inflearn/8-12.js
+++ inflearn/8-12.js
@@ -6,7 +6,7 @@ function solution(n, r) {
 
   function DFS(n, r) {
     if (r === 1) return n;
-    if (r === n) return 1;
+    if (r === n) return 1; // 이 두개를 합쳐서 if(n === r || r === 0) return 1; 로 할 수 있음
 
     return DFS(n - 1, r - 1) + DFS(n - 1, r);
   }
