557b6dbc72028fd3bbdb34135af98f6e01265086
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-07-06 00:07:17 +0900

    8-15) 답안 풀이

--- inflearn/8-15.js
+++ inflearn/8-15.js
@@ -18,5 +18,24 @@ function solution(n, k, arr, m) {
   return answer;
 }
 
+// 답안 풀이
+function solution2(n, k, arr, m) {
+  let answer = 0;
+
+  function DFS(L, s, sum) {
+    if (L === k && sum % m === 0) {
+      answer += 1;
+      return;
+    }
+    for (let i = s; i <= n; i++) {
+      DFS(L + 1, i + 1, sum + arr[i]);
+    }
+  }
+
+  DFS(0, 0, 0);
+  return answer;
+}
+
 let arr = [2, 4, 5, 8, 12];
 console.log(solution(5, 3, arr, 6));
+console.log(solution2(5, 3, arr, 6));
