7952555b6ade9ff44e7b252c03ef4216944e2a4e
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-09-25 09:42:08 +0900

    (수정) K개 중 하나를 N번 선택하기(Simple) / 아름다운 수

--- codetree/beautiful-number.js
+++ codetree/beautiful-number.js
@@ -8,42 +8,38 @@ let input = fs.readFileSync(filePath).toString().trim().split("\n");
 let answer = 0;
 
 const n = +input[0];
-const check = Array.from({ length: 5 }, () => 0);
+const number = [];
 
-let number = [];
-function select(L, num) {
-  console.log("all", number, check);
-  if (L === n) {
-    if (
-      check.filter(
-        (val, idx) =>
-          (idx === n && val === idx) ||
-          (idx === n && val === 0) ||
-          (idx !== n && val === idx) ||
-          (idx !== n && val === 0)
-      ).length === 5
-    ) {
-      console.log(number, check);
-      answer++;
+const isBeutiful = (num) => {
+  for (let i = 0; i < n; ) {
+    let checkNum = num[i];
+    let count = num[i];
+    while (count) {
+      if (num[i] !== checkNum) return false;
+      count--;
+      i++;
     }
+  }
+  return true;
+};
+
+function select(L, num) {
+  if (L === n) {
+    if (isBeutiful(number.join(""))) answer++;
     return;
   }
+
   for (let i = 1; i <= 4; i++) {
-    if (check[i] === i) continue;
-    check[i]++;
     number.push(i);
     select(L + 1, i);
     number.pop(i);
-    check[i]--;
   }
 }
 
 for (let i = 1; i <= 4; i++) {
-  check[i]++;
   number.push(i);
   select(1, i);
   number.pop(i);
-  check[i]--;
 }
 
 console.log(answer);
